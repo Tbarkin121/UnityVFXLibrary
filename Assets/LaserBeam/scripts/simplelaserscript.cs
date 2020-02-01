68eba416599d4919cbeba19cb2f7c55f3b913e79
Tyler Barkin <tybarkin@gmail.com>
2020-02-01 17:27:32 -0500

    Adding the current state of my VFX Library

---
+++ Assets/LaserBeam/scripts/simplelaserscript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simplelaserscript : MonoBehaviour
+{
+    public GameObject laserPrefab;
+    public GameObject firePoint;
+    private GameObject spawnedLaser;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnedLaser = Instantiate(laserPrefab, firePoint.transform) as GameObject;
+        DisableLaser();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            EnableLaser();
+        }
+        if(Input.GetMouseButton(0))
+        {
+            UpdateLaser();
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            DisableLaser();
+        }
+        
+    }
+
+    void EnableLaser()
+    {
+        spawnedLaser.SetActive(true);
+    }
+    void DisableLaser()
+    {
+        spawnedLaser.SetActive(false);
+    }
+    void UpdateLaser()
+    {
+        if(firePoint != null)
+        {
+            spawnedLaser.transform.position = firePoint.transform.position;
+        }
+    }
+}
